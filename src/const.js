10c8da02b52539fa8f14a8e774ca6e9613b3df31
aisevim <a.aydnlp.pro@gmail.com>
2023-11-17 13:57:01 +0100

    fee: asd

--- src/const.js
+++
@@ -1,1 +0,0 @@
-export const REGEX_COMMIT = /^(wip|Merge?.+|Revert?.+)|(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|release)(\([-A-Za-z0-9_ ]+\))?!?: .+/
--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
-import core from '@actions/core'
-import github from '@actions/github';
+import core from "@actions/core"
+import github from "@actions/github"
 
-import { REGEX_COMMIT } from './const';
+async function run() {
+  try {
+    const octokit = github.getOctokit(core.getInput('github-token')); // Tu dois fournir un token d'accès GitHub
+    const commitRegex = new RegExp(core.getInput('commit-regex'));
+    const maxCommitsPerPage = 100
+    let page = 1
+    let commits = []
 
-try {
-  const commitMessage = github.context.payload.head_commit.message;
-	const commitRegex = core.getInput('commit-regex') || REGEX_COMMIT;
+    async function getCommits(page) {
+      const commitsInfo = await octokit.rest.pulls.listCommits({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: github.context.payload.pull_request?.number,
+        per_page: maxCommitsPerPage,
+        page
+      });
 
-  if (!commitRegex.test(commitMessage)) {
-    core.setFailed('The commit message does not adhere to the expected format.');
-    core.warning('The commit message should have the format:\n');
-    core.warning('<type> [<optional scope>]: <description>\n');
-    core.warning('body\n');
-    core.warning('footer');
+      commits.push(...commitsInfo.data)
+
+      if (commitsInfo.length === maxCommitsPerPage) {
+        page++
+        commits.push(...(await getCommits(page)))
+      }
+
+      return commits
+    }
+
+    if (github.context.eventName === 'pull_request') {
+      const commitsInfo = await getCommits(page)
+      const commitInfo = commitsInfo?.at(-1)
+      const commitMessage = commitInfo?.commit?.message
+
+      if (commitMessage && !commitRegex.test(commitMessage)) {
+        core.setFailed('The commit message does not adhere to the expected format.');
+        core.warning(`The commit message should have the format:
+        <type> [<optional scope>]: <description>
+
+        [<optional body>]
+
+        [<optional footer>]
+        `);
+      }
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
