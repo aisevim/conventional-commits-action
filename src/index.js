c519df1250be323b0cda461e711bd06f312e7bed
aisevim <a.aydnlp.pro@gmail.com>
2023-11-21 19:14:13 +0100

    feat

--- src/index.js
+++ src/index.js
@@ -3,48 +3,71 @@ import github from "@actions/github"
 
 import { checkCommitMessages } from "./checker.js"
 
+const maxCommitsPerPage = 100
+
 async function run() {
+  if (github.context.eventName !== 'pull_request') {
+    return
+  }
+
   try {
-    const octokit = github.getOctokit(core.getInput('github-token')); // Tu dois fournir un token d'accès GitHub
-    const maxCommitsPerPage = 100
+    const octokit = github.getOctokit(core.getInput('github-token'));
+    const hasTitlePR = core.getInput('has-title-pr')
+    const hasCommits = core.getInput('has-commits')
+    const onPRTitleChange = hasTitlePR && github.context.payload.action === 'edited'
+    const onCommitChange = hasCommits && (
+      github.context.payload.action === 'opened' ||
+      github.context.payload.action === 'synchronize' ||
+      github.context.payload.action === 'reopened'
+    )
     let page = 1
-    let commits = []
+    let text = ''
 
-    async function getCommits(page) {
-      const commitsInfo = await octokit.rest.pulls.listCommits({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: github.context.payload.pull_request?.number,
-        per_page: maxCommitsPerPage,
-        page
-      });
-
-      commits.push(...commitsInfo.data)
-
-      if (commitsInfo.length === maxCommitsPerPage) {
-        page++
-        commits.push(...(await getCommits(page)))
-      }
-
-      return commits
+    if (onPRTitleChange) {
+      text = github.context.payload.pull_request?.title ?? ''
+      generateLog(text, 'pr-title')
     }
 
-    if (github.context.eventName === 'pull_request') {
-      let text = ''
+    if (onCommitChange) {
+      const commitsInfo = await getCommits(octokit, page)
+      const commitInfo = commitsInfo?.at(-1)
+      text = commitInfo?.commit?.message ?? ''
+      generateLog(text, 'pr-commit')
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
 
-      if(github.context.payload.action === 'edited') {
-        text = github.context.payload.pull_request?.title;
-      } else {
-        const commitsInfo = await getCommits(page)
-        const commitInfo = commitsInfo?.at(-1)
-        text = commitInfo?.commit?.message
-      }
-      const [isCommitInvalid, log] = checkCommitMessages(text)
+async function getCommits(octokit, page) {
+  let commits = []
+  
+  const commitsInfo = await octokit.rest.pulls.listCommits({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload.pull_request?.number,
+    per_page: maxCommitsPerPage,
+    page
+  });
 
-      if (text && isCommitInvalid) {
-        core.setFailed('The commit message does not adhere to the expected format.');
-        core.warning(log);
-        core.info(`Conventional Commits provide a standardized format for commit messages, enabling better collaboration among developers, automating the release process, and generating comprehensive changelogs.
+  commits.push(...commitsInfo.data)
+
+  if (commitsInfo.length === maxCommitsPerPage) {
+    page++
+    commits.push(...(await getCommits(page)))
+  }
+
+  return commits
+}
+
+function generateLog(text, type) {
+  const [isCommitInvalid, log] = checkCommitMessages(text)
+  const logType = type === 'pr-title' ? 'PR title' : 'commit message'
+
+  if (text && isCommitInvalid) {
+    core.setFailed(`The ${logType} does not adhere to the expected format.`);
+    core.warning(log);
+    core.info(`Conventional Commits provide a standardized format for commit messages, enabling better collaboration among developers, automating the release process, and generating comprehensive changelogs.
 
 The structure of a Conventional Commit message typically follows this format:
 <type>(<scope>): <description>
@@ -57,11 +80,6 @@ The structure of a Conventional Commit message typically follows this format:
 <description>: Briefly explains the change introduced in the commit.
 <body> (optional): Provides additional context, details, or reasoning behind the change.
 <footer> (optional): Includes information like issue tracker references or breaking changes.`);
-      }
-    }
-
-  } catch (error) {
-    core.setFailed(error.message);
   }
 }
 
